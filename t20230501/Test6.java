3850d9e5a604b5f90a62ddac4d3c717e60459d65
noo-o <zxzx00202@naver.com>
2023-05-01 16:18:29 +0900

    참조를 반환하는 메서드

---
+++ t20230501/Test6.java
@@ -0,0 +1,18 @@
+package t20230501;
+
+public class Test6 {
+
+	public static void main(String[] args) {
+		Day d=new Day(2010, 10, 15);
+		Account gildong = new Account("길동", "125768", 100, d);
+		
+		Day p = gildong.getOpenDay();
+		System.out.println("계좌 개설일: "+p);
+		
+		p.set(1999.12.31);
+		
+		Day q= gildong.getOpenDay();
+		System.out.println("계좌 개설일:"+q);
+	}
+
+}
