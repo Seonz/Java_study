379b1b3b6c186865e2e04cf027aa400fbd7edc69
noo-o <zxzx00202@naver.com>
2023-05-04 14:37:01 +0900

    is -A와 인스턴스 참조

---
+++ t20230504/CarTester1.java
@@ -0,0 +1,23 @@
+package t20230504;
+
+public class CarTester1 {
+
+	public static void main(String[] args) {
+	//	Car car1 = new Car("W140",1885,1490,5220,95.0,new Day (2018,10,13));
+	//	ExCar car2 = new ExCar("W221",1845,1490,5205,90.0,new Day(2015,12,24));
+		
+		
+	//	Car x;
+	//	c=car1;
+	//	x=car2;
+		
+	//	System.out.println("x구입일:"+x.getPurchaseDay());
+		
+	//	ExCar y;
+	//	y=car2;
+		
+	//	System.out.println("Y구입일:"+y.getpurchaseDay());
+	//	System.out.println("y의 총 주행 거리:"+y.getTotalMileage());
+	}
+
+}
